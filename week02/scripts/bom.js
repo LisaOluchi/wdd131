08727af21bd1c3dedc14e198f1ea597ddcaa9eba
LisaOluchi <sangqulisa@gmail.com>
2024-05-08 13:41:40 +0200

    W02 Prep Act PT2

---
+++ week02/scripts/bom.js
@@ -0,0 +1,32 @@
+
+        
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list'); 
+
+button.addEventListener('click', () => {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+
+    input.value = '';
+    input.focus();
+  } else {
+    
+    input.focus();
+  }
+});
+
+
