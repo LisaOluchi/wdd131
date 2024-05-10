d8bbf2b433689a20df3979f49d872d5530c78586
LisaOluchi <sangqulisa@gmail.com>
2024-05-10 19:56:56 +0200

    pics added

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,18 +2,20 @@
 const hamburgerBtn = document.querySelector('.hamburger');
 const navMenu = document.querySelector('nav ul');
 
-
 function toggleMenu() {
   navMenu.classList.toggle('show');
 }
 
 hamburgerBtn.addEventListener('click', toggleMenu);
 
+window.addEventListener('resize', function() {
+  if (window.innerWidth > 768) {
+    navMenu.classList.remove('show');
+  }
+});
 
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
-
 const lastModifiedDate = document.lastModified;
 document.getElementById("lastModified").textContent = "Last modified: " + lastModifiedDate;
-
